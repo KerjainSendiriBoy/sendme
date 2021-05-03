0594abb5f68855961e70bb6e915d2451a4c686f3
eeRha <arrasyidtz3@gmail.com>
2021-05-04 00:33:46 +0700

    unit testing

---
+++ app/src/androidTest/java/com/example/sendme/activity/LoginActivityTest.java
@@ -0,0 +1,22 @@
+package com.example.sendme.activity;
+
+import junit.framework.TestCase;
+
+public class LoginActivityTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testOnCreate() {
+    }
+
+    public void testSetWindowFlag() {
+    }
+
+    public void testOnBackPressed() {
+    }
+}
---
+++ app/src/test/java/com/example/sendme/activity/BusActivityTest.java
@@ -0,0 +1,35 @@
+package com.example.sendme.activity;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BusActivityTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void onCreate() {
+    }
+
+    @Test
+    public void onOptionsItemSelected() {
+    }
+
+    @Test
+    public void perhitunganHarga() {
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void submitBtn() {
+    }
+}
